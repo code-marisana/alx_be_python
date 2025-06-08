ebec27ca7b7efbaf95d0d95144f763a77be2203c
code-marisana <marisana@marisanamasha.com>
2025-06-08 16:50:47 +0200

    imports fix

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,6 +1,7 @@
+from datetime import date
 import datetime
 def display_current_datetime():
     current_date = datetime.datetime.today()
     print(f"Current date and time: {current_date.year}-{current_date.strftime("%m")}-{current_date.strftime("%d")} {current_date.strftime("%H")}:{current_date.strftime("%M")}:{current_date.strftime("%S")}")
-    
+
 display_current_datetime()
