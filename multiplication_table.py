8331b4c1fdd4265d83bc9ebd244376e2fbd1c0b7
code-marisana <marisana@marisanamasha.com>
2025-06-08 13:09:45 +0200

    add arithmetic operations

--- multiplication_table.py
+++
@@ -1,4 +0,0 @@
-number = int(input("Enter a number to see its multiplication table: "))
-iterations = range(1,11)
-for iteration in iterations:
-    print(f"{number} * {iteration} = {number * iteration}")
