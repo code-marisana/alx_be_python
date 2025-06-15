191b0c514166c0c2a9b4cff5c59728500989d905
code-marisana <marisana@marisanamasha.com>
2025-06-15 14:57:29 +0200

    add False and True

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -8,6 +8,8 @@ class BankAccount:
     def withdraw(self, amount):
         if self.account_balance >= amount:
             self.account_balance -= amount
-    
+            return True
+        else:
+            return False
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance}")
