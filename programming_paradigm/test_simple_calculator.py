808952d89293309b8fbbe536350c7e69ac2484ed
code-marisana <marisana@marisanamasha.com>
2025-06-15 16:04:08 +0200

    sfsgdg

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,13 +6,13 @@ class TestCalculator(unittest.TestCase):
         """Set up the SimpleCalculator instance before each test."""
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(2,3), 5)
         self.assertEqual(self.calc.add(-1,1), 0)
         self.assertEqual(self.calc.add(-1,-1), -2)
         self.assertEqual(self.calc.add(1,-1), 0)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(3,2), 1)
         self.assertEqual(self.calc.subtract(2,2), 0)
         self.assertEqual(self.calc.subtract(1,-1), 2)
