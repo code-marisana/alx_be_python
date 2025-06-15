b256bf091a35a7e246a017151a8a8b65f366a7ee
code-marisana <marisana@marisanamasha.com>
2025-06-15 18:12:13 +0200

    methods to both objects

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -4,31 +4,37 @@ class Book:
         self.author = author
         self._is_checked_out = False
 
+    def return_book(self):
+            if self._is_checked_out:
+                    self._is_checked_out = False
+            else:
+                print(f"Book already returned!")
+
+    def check_out_book(self):
+            if self._is_checked_out:
+                print(f"{self.title} is unavailable")
+            else:
+                self._is_checked_out = True
+
 class Library:
     def __init__(self):
         self._books = []
     
     def add_book(self,book):
         self._books.append(book)
+          
     
-    def check_out_book(self, book_title):
-        for book in self._books:
-            if book.title == book_title:
-                if book._is_checked_out:
-                    print(f"{book_title} is unavailable")
-                else:
-                    book._is_checked_out = True
-
-    def return_book(self, book_title):
-        for book in self._books:
-            if book.title == book_title:
-                if book._is_checked_out:
-                    book._is_checked_out = False
-                else:
-                    print(f"Book already returned!")
-    
-
     def list_available_books(self):
         for book in self._books:
             if not book._is_checked_out:
                 print(f"{book.title} by {book.author}")
+
+    def return_book(self, title):
+         for book in self._books:
+              if book.title == title:
+                   book.return_book()
+
+    def check_out_book(self, title):
+         for book in self._books:
+              if book.title == title:
+                   book.check_out_book()
