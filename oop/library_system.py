5f475ee50653d5a0e7fab04eea98ec96f20b85c7
code-marisana <marisana@marisanamasha.com>
2025-06-22 12:59:37 +0200

    add library system

---
+++ oop/library_system.py
@@ -0,0 +1,29 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+    
+class EBook(Book):
+    def __init__(self, file_size):
+        self.file_size = file_size
+
+class PrintBook(Book):
+    def __init__(self, page_count):
+        self.page_count = page_count
+
+
+class Library:
+    def __init__(self):
+        self.books = []
+    
+    def add_book(self, book):
+        self.books.append(book)
+
+    def list_books(self):
+        for book in self.books:
+            if book == isinstance(EBook):
+                print(f"EBook: {book.title} by {book.author}, File Size: {book.file_size}KB")
+            elif book == isinstance(PrintBook):
+                print(f"PrintBook: {book.title} by {book.author}, Page Count: {book.page_count}")
+            else:
+                print(f"{book.title} by {book.author}")
